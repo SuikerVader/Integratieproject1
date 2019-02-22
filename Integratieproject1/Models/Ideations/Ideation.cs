22c4d64554e528e09cbe0556827ad0db56765c41
SuikerVader <jesse.vanloock@student.kdg.be>
2019-02-22 11:36:10 +0100

    Added necessary classes to Ideations and Surveys

---
+++ Integratieproject1/Models/Ideations/Ideation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integratieproject1.Models
+{
+    public class Ideation
+    {
+    }
+}
