565f49274da124b48a1aa17b9e2042370d81c59e
SuikerVader <jesse.vanloock@student.kdg.be>
2019-02-22 11:51:29 +0100

    Greetings added in Program.cs

--- Integratieproject1/Program.cs
+++ Integratieproject1/Program.cs
@@ -15,6 +15,7 @@ namespace Integratieproject1
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            Console.WriteLine("Hallo iedereen!");
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
