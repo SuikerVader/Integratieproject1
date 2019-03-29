a6c94dcf12f389c4eea3c7e24795878b2c84a72d
jeroen1323 <jeroen.dehoon@student.kdg.be>
2019-03-29 11:46:59 +0100

    PostIdea toegevoegd

--- Integratieproject1/wwwroot/js/site.js
+++ Integratieproject1/wwwroot/js/site.js
@@ -14,4 +14,4 @@ import 'bootstrap/dist/css/bootstrap.css'
 // Custom CSS imports
 import '../css/site.css'
 
-console.log('The \'site\' bundle has been loaded!')
+console.log('The \'site\' bundle has been loaded!');
