5d10a9cefd99ef2f46ee8ea2ecfc449d2ac7b644
SuikerVader <jesse.vanloock@student.kdg.be>
2019-03-29 11:35:17 +0100

    Identity Added

---
+++ Integratieproject1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Integratieproject1.DAL;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Integratieproject1.Areas.Identity.IdentityHostingStartup))]
+namespace Integratieproject1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CityOfIdeasDbContext>(options =>
+                    options.UseSqlite("Data Source=CityOfIdeas.db"));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<CityOfIdeasDbContext>();
+            });
+        }
+    }
+}
