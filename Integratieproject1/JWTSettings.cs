1b3e622d7f33df8d7a3994947517df44c0781496
jeroen1323 <jeroen.dehoon@student.kdg.be>
2019-05-18 11:09:41 +0200

    Token based login voor Android verwijderd

--- Integratieproject1/JWTSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Integratieproject1
-{
-    public class JWTSettings
-    {
-        public string SecretKey { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-    }
-}
