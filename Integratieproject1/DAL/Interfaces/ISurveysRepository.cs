2bb52043e8b765cf28d776f7bdb5ba19b94e9066
daanjac <34395881+daanjac@users.noreply.github.com>
2019-05-23 21:20:06 +0200

    Daan ja (#130)
    * merge
    
    * responsive
    
    * ui changes
    
    * ui changes
    
    * home page
    
    * home page
    
    * merge
    
    * add idea
    
    * add idea
    
    * refactoring
    
    * overzicht van ideas voor users
    
    * merge
    
    * refactoring interfaces
    
    * refactoring
    
    * small fix

--- Integratieproject1/DAL/Interfaces/ISurveysRepository.cs
+++ Integratieproject1/DAL/Interfaces/ISurveysRepository.cs
@@ -5,15 +5,39 @@ namespace Integratieproject1.DAL.Interfaces
 {
     public interface ISurveysRepository
     {
-        IEnumerable<Survey> GetSurveys(int phaseId);
+       #region Survey
+
+       IEnumerable<Survey> GetSurveys(int phaseId);
+        IEnumerable<Survey> GetProjectSurveys(int projectId);
+        IEnumerable<Survey> GetAllSurveys();
+        Survey GetOnlySurvey(int surveyId);
         Survey GetSurvey(int surveyId);
         Survey CreateSurvey(Survey survey);
+        Survey EditSurvey(Survey survey);
+        void RemoveSurvey(Survey survey);
+        bool IsEmail(int id, int key);
+        
+        #endregion
+        
+        #region Question
+
         IEnumerable<Question> GetQuestions(int surveyId);
         Question GetQuestion(int questionId);
+        Question EditQuestion(Question question);
         Question CreateQuestion(Question question);
+        void RemoveQuestion(Question question);
+        
+        #endregion
+
+        #region Answer
+        // Answer methods
         IEnumerable<Answer> GetAnswers();
         Answer GetAnswer(int answerId);
         Answer CreateAnswer(Answer answer);
+        Answer EditAnswer(Answer answer);
         Answer UpdateAnswer(Answer answer);
+        void RemoveAnswer(Answer answer);
+
+        #endregion
     }
 }
